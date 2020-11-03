21f6f617836747cce3638bf0a36913fb5d41bdd3
Scott <scott@scott-liu.com>
2020-11-03 17:47:47 -0500

    changed cudnn to version 7.6

--- ml_env.sh
+++ ml_env.sh
@@ -4,9 +4,9 @@ sudo dpkg -i nvidia-machine-learning-repo-ubuntu1804_1.0.0-1_amd64.deb
 sudo apt update
 sudo apt install libnccl2=2.7.8-1+cuda10.2 libnccl-dev=2.7.8-1+cuda10.2
 rm nvidia-machine-learning-repo-ubuntu1804_1.0.0-1_amd64.deb
-wget http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1804/x86_64/libcudnn8_8.0.4.30-1+cuda10.2_amd64.deb
-sudo dpkg -i libcudnn8_8.0.4.30-1+cuda10.2_amd64.deb
-rm libcudnn8_8.0.4.30-1+cuda10.2_amd64.deb
-wget http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1804/x86_64/libcudnn8-dev_8.0.4.30-1+cuda10.2_amd64.deb
-sudo dpkg -i libcudnn8-dev_8.0.4.30-1+cuda10.2_amd64.deb
-rm libcudnn8-dev_8.0.4.30-1+cuda10.2_amd64.deb
+wget http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1804/x86_64/libcudnn7_7.6.5.32-1+cuda10.2_amd64.deb
+sudo dpkg -i libcudnn7_7.6.5.32-1+cuda10.2_amd64.deb
+rm libcudnn7_7.6.5.32-1+cuda10.2_amd64.deb
+wget http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1804/x86_64/libcudnn7-dev_7.6.5.32-1+cuda10.2_amd64.deb
+sudo dpkg -i libcudnn7-dev_7.6.5.32-1+cuda10.2_amd64.deb
+rm libcudnn7-dev_7.6.5.32-1+cuda10.2_amd64.deb
