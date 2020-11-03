5b0d7e6cbf0f0892867bea34af03979ed66c48c4
Scott <scott@scott-liu.com>
2020-11-03 21:59:31 +0000

    separated out non-sudo installations

---
+++ install_mods.sh
@@ -0,0 +1,4 @@
+# install python2 modules
+python2 -m pip install --upgrade pip
+pip2 install --user jupyter ipython matplotlib scikit-image protobuf easydict opencv-python==4.2.0.32 pyyaml
+python2 -m ipykernel install --user
