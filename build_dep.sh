76dd0e3d137ad54c68b463b5a9b95c1f371e042a
Scott <scott@scott-liu.com>
2020-11-03 22:27:54 +0000

    removed python modules from dependency script

--- build_dep.sh
+++ build_dep.sh
@@ -33,7 +33,3 @@ make
 cd $REPO_ROOT/lib
 make
 
-# install python2 modules
-python2 -m pip install --upgrade pip
-pip2 install jupyter ipython matplotlib scikit-image protobuf easydict opencv-python==4.2.0.32 pyyaml
-python2 -m ipykernel install --user
