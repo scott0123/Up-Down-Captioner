3ad84c36794d5226a37989c3a6ea6af2df897ca3
Scott <scott@scott-liu.com>
2020-11-03 22:24:06 +0000

    Added missing cudnn-dev package

--- ml_env.sh
+++ ml_env.sh
@@ -7,3 +7,6 @@ rm nvidia-machine-learning-repo-ubuntu1804_1.0.0-1_amd64.deb
 wget http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1804/x86_64/libcudnn8_8.0.4.30-1+cuda10.2_amd64.deb
 sudo dpkg -i libcudnn8_8.0.4.30-1+cuda10.2_amd64.deb
 rm libcudnn8_8.0.4.30-1+cuda10.2_amd64.deb
+wget http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1804/x86_64/libcudnn8-dev_8.0.4.30-1+cuda10.2_amd64.deb
+sudo dpkg -i libcudnn8-dev_8.0.4.30-1+cuda10.2_amd64.deb
+rm libcudnn8-dev_8.0.4.30-1+cuda10.2_amd64.deb
